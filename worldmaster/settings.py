e9d555b15cc1098044cfe709b326e73f9b9d1b01
Brandon Phillips <btp@brandont.dev>
2023-01-04 09:43:37 -0700

    Added .gitignore and .env

--- worldmaster/settings.py
+++ worldmaster/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-89cm+i#h&1^%08xcfv#jk2*d*!xe$ag18q0(*w#=*ufgb$9w#&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
