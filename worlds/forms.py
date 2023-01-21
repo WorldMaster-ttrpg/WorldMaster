77a085cd73cf087f350cad4d7f1276873a216d4d
Taylor C. Richberger <taywee@gmx.com>
2023-01-20 23:43:38 -0700

    get all the auto slug stuff working, also add django_extensions for QoL

--- worlds/forms.py
+++ worlds/forms.py
@@ -1,12 +1,38 @@
-from django.forms import ModelForm
+from django import forms
 from worlds.models import World, Plane
+from django.template.defaultfilters import slugify
+from django.utils.translation import gettext as _
 
-class WorldForm(ModelForm):
+class WorldForm(forms.ModelForm):
     class Meta:
         model = World
-        fields = ['name', 'slug', 'description']
+        # Even though slug is a computed field, we want to run validation on
+        # it.  The easiest way of doing that is keeping it as a useless hidden
+        # field.  Without this, validation needs to be done manually and
+        # doesn't reflect well in the form itself.
+        fields = ['name', 'slug']
 
-class PlaneForm(ModelForm):
+    # To allow new forms to not barf on validation before field can be cleaned.
+    slug = forms.SlugField(required=False, widget=forms.HiddenInput())
+
+    def clean_slug(self):
+        return slugify(self.cleaned_data['name'])
+
+class PlaneForm(forms.ModelForm):
     class Meta:
         model = Plane
-        fields = ['name', 'slug', 'description']
+        fields = ['name', 'slug']
+        widgets = {'slug': forms.HiddenInput()}
+
+    # To allow new forms to not barf on validation before field can be cleaned.
+    slug = forms.SlugField(required=False)
+
+    def __init__(self, *args, world: World | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if world is not None:
+            self.instance.world = world
+
+        assert self.instance.world is not None
+
+    def clean_slug(self):
+        return slugify(self.cleaned_data['name'])
--- worlds/models.py
+++ worlds/models.py
@@ -1,12 +1,32 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
+from django.template.defaultfilters import slugify
 from django.urls import reverse
+from typing import Protocol
 from worldmaster.validators import validate_not_reserved
 
 User = get_user_model()
 
-class World(models.Model):
+class Sluggable(models.Model):
+    slug = models.SlugField(
+        null=False,
+        blank=False,
+        max_length=256,
+        validators=[MinLengthValidator(3), validate_not_reserved],
+    )
+
+    name = models.CharField(
+        null=False,
+        blank=False,
+        max_length=256,
+        validators=[MinLengthValidator(3)],
+    )
+
+    class Meta:
+        abstract = True
+
+class World(Sluggable):
     '''The top-level model for a setting, representing an entire game setting,
     including all planes, and encompassing all history.
 
@@ -14,31 +34,21 @@ class World(models.Model):
     "multiverse".
     '''
 
-    # The URL slug for this world
-    slug = models.SlugField(
-        db_index=True,
-        unique=True,
-        null=False,
-        blank=False,
-        max_length=64,
-        validators=[MinLengthValidator(3), validate_not_reserved],
-    )
-
-    name = models.CharField(
-        null=False,
-        blank=False,
-        max_length=64,
-        validators=[MinLengthValidator(3)],
-    )
-    description = models.TextField(null=False, blank=True)
-
     created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta(Sluggable.Meta):
+        constraints = [
+            models.UniqueConstraint(fields=['slug'], name='unique_world_slug'),
+        ]
 
     def get_absolute_url(self) -> str:
         return reverse('worlds:world', kwargs={'world_slug': self.slug})
 
-class Plane(models.Model):
+    def __str__(self) -> str:
+        return self.slug
+
+class Plane(Sluggable):
     '''A single dimension, with a set of entities set in physical coordinates.
 
     This is a single physical universe.
@@ -46,30 +56,16 @@ class Plane(models.Model):
 
     world = models.ForeignKey(World, null=False, blank=False, on_delete=models.CASCADE)
 
-    slug = models.SlugField(
-        null=False,
-        blank=False,
-        max_length=64,
-        validators=[MinLengthValidator(3), validate_not_reserved],
-    )
-
-    name = models.CharField(
-        null=False,
-        blank=False,
-        max_length=64,
-        validators=[MinLengthValidator(3)],
-    )
-
-    description = models.TextField(null=False)
-
     created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['world', 'slug'], name='unique_world_slug'),
+            models.UniqueConstraint(fields=['world', 'slug'], name='unique_plane_world_slug'),
         ]
 
     def get_absolute_url(self) -> str:
         return reverse('worlds:plane', kwargs={'world_slug': self.world.slug, 'plane_slug': self.slug})
 
+    def __str__(self) -> str:
+        return self.slug
