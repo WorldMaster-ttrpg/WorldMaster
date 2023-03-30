3c8a481aee30ad0960cc519f29190f445134e7ee
Taylor C. Richberger <taywee@gmx.com>
2023-03-30 08:33:16 -0600

    get wiki saving for world

--- src/worlds/views/world.py
+++ src/worlds/views/world.py
@@ -2,10 +2,12 @@ from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpR
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import TemplateView, ListView, DetailView, View
 from django.views.decorators.http import require_http_methods
-from wiki.models import Article
+from django.db import transaction
+from wiki.models import Article, Section
 from worlds.models import World
 from worlds.forms import WorldForm
 from copy import copy
+import json
 
 class WorldsView(ListView):
     model = World
@@ -40,14 +42,52 @@ class EditWorldView(View):
         return HttpResponse(render(request, self.template_name, {'form': form, 'world': world}))
 
     def post(self, request: HttpRequest, world_slug: str) -> HttpResponse:
-        world: World = get_object_or_404(World, slug=world_slug)
+        with transaction.atomic():
+            world: World = get_object_or_404(World, slug=world_slug)
 
-        # Copy the object in so that the bad request page doesn't get the
-        # modified instance. If you don't do this and you try to change the
-        # slug, even if it fails, the form action will be changed to match
-        # the attempt.
-        form = WorldForm(request.POST, instance=copy(world))
-        if form.is_valid():
-            return redirect(form.save())
-        else:
-            return HttpResponseBadRequest(render(request, self.template_name, {'form': form, 'world': world}))
+            # Copy the object in so that the bad request page doesn't get the
+            # modified instance. If you don't do this and you try to change the
+            # slug, even if it fails, the form action will be changed to match
+            # the attempt.
+            form = WorldForm(request.POST, instance=copy(world))
+            if form.is_valid():
+                instance = form.save()
+                # Now save the wiki.
+                # TODO: Move this somewhere reusable, like in the Article model,
+                # or even an Article view.  Perhaps a sub-view thing, maybe with
+                # a sub-form object for better composeability.
+                article: Article = world.article
+
+                # Not a set or dict because None may appear multiple times
+                print(request.POST)
+
+                def load_int(value: str) -> int | None:
+                    '''Load an integer if the string is not empty, otherwise None.
+                    '''
+                    if value:
+                        return int(value)
+                    else:
+                        return None
+
+                section_ids = tuple(map(load_int, request.POST.getlist('wiki-section-id')))
+
+                section_orders = map(float, request.POST.getlist('wiki-section-order'))
+                sections = request.POST.getlist('wiki-section')
+
+                present_ids = set(id for id in section_ids if id is not None)
+
+                for id, order, text in zip(section_ids, section_orders, sections):
+                    if id is None:
+                        section = article.section_set.create(order=order, text=text)
+                        present_ids.add(section.id)
+                    else:
+                        section: Section = get_object_or_404(Section, id=id)
+                        section.text = text
+                        section.order = order
+                        section.save()
+
+                # Delete removed sections
+                article.section_set.exclude(id__in=present_ids).delete()
+                return redirect(instance)
+            else:
+                return HttpResponseBadRequest(render(request, self.template_name, {'form': form, 'world': world}))
